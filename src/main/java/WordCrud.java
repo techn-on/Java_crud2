24d60bb11ee553a106e6606c41a2767b7364f988
onsunghyun <22100457@handong.ac.kr>
2022-09-17 15:06:00 +0900

    updateitem make

---
+++ src/main/java/WordCrud.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class WordCrud implements ICRUD{
+    ArrayList<Word> list;
+    Scanner s;
+    WordCrud(Scanner s) {
+        list= new ArrayList<>();
+        this.s=s;
+    }
+    @Override
+
+        public Object add(){
+            System.out.print("=> 난이도 (1,2,3) & 새 단어 입력 : ");
+            int level =s.nextInt();
+            String word =s.nextLine();
+            System.out.print("뜻 입력 : ");
+            String meaning= s.nextLine();
+            return new Word(0,level,word,meaning);
+
+        }
+        public  void addWord() {
+        Word one = (Word) add();
+        list.add(one);
+        System.out.println("\n새 단어장에 단어가 추가 되었습니다. ");
+        }
+    @Override
+    public int update(Object obj){
+        this.obj.level= s.
+                //contains
+                //printf
+
+    }
+    @Override
+    public int delete(Object obj){
+        return 0;
+
+    }
+    @Override
+    public void selectOne(int id){
+
+
+    }
+
+    public void listAll(){
+        System.out.println("---------------------------------");
+        for(int i=0; i<list.size();i++ ){
+            System.out.print((i+1)+" ");
+            System.out.println(list.get(i).toString());
+        }
+        System.out.println("---------------------------------");
+    }
+    public ArrayList<Integer> listAll(String keyword){
+        ArrayList<Integer> idList =new ArrayList<>();
+        int j=0;
+        System.out.println("---------------------------------");
+        for(int i=0; i<list.size();i++ ){
+          String word =list.get(i).getWord();
+          if(word.contains(keyword)){
+              System.out.print((j+1)+" ");
+              System.out.println(list.get(i).toString());
+              idList.add(i);
+              j++;
+          }
+        }
+        System.out.println("---------------------------------");
+        return idList;
+    }
+
+
+    public void updateItem() {
+        System.out.print("=> 수정할 단어 검색 : ");
+        String keyword =s.next();
+        ArrayList<Integer > idList =this.listAll(keyword);
+        System.out.print("=> 수정할 번호 선택 :");
+        int id = s.nextInt();
+        s.nextLine();
+        System.out.print("=> 뜻 입력 :");
+        String meaning = s.nextLine();
+        Word word =list.get(idlist.get(id-1));
+        word.setMeaning(meaning);
+        System.out.println("단어가 수정되었습니다. ");
+    }
+}
